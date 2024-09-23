454fd27ca27a7b1924b190c9c185b4dd9609bc07
CarolGitonga <carolgitongaofficial@gmail.com>
2024-09-23 16:20:04 +0300

    style the input and the search button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    HousingLocationComponent
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
