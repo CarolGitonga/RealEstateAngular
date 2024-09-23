02714a66147c2541c4bdae94bb3a5843a9642b97
CarolGitonga <carolgitongaofficial@gmail.com>
2024-09-23 16:53:24 +0300

    create an interface to let the component know the type of data to expect

---
+++ src/app/housing-location.ts
@@ -0,0 +1,10 @@
+export interface HousingLocation {
+    id: number;
+    name: string;
+    city: string;
+    area: string;
+    photo: string;
+    availableUnits: number;
+    wifi: boolean;
+    laundry: boolean;
+}
