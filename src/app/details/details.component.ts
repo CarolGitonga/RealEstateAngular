04df68ba015167e2bce9a264cc9129da3070a093
CarolGitonga <carolgitongaofficial@gmail.com>
2024-09-24 13:11:23 +0300

    Retrieve information from a route in a component using the ActivatedRoute feature

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../housing-location';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+  }
+
+}
